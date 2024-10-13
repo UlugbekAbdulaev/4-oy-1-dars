4e444c6d691d209a21e87ae942be6d94cabfb637
ulugbekAbdullayev <ulu23503@gmail.com>
2024-10-14 01:16:14 +0500

    4oy 1dars

---
+++ java.js
@@ -0,0 +1,81 @@
+  
+
+// 1-masala
+// let a= +prompt('kvadratni tomoni')
+// console.log(a)
+// let p 
+// p=4*a 
+// console.log("peremetr = " + p )
+
+
+//2 masala
+// let a= +prompt ('kvadratning tomoni')
+// let s 
+// s= a*a 
+// console.log("yuzasi = " + s )
+
+
+//3-masala
+// let a=+prompt ('togri tortburchakning tomoni')
+// let b=+prompt ('togri tortburchakning tomoni')
+// let s 
+// s=a*b
+// console.log('yuzasi - ' + s)
+// let p 
+// p=2*(a+b)
+// console.log('peremetri - ' + p)
+
+
+//4-masala
+// let d=+prompt('aylananing diametrini kiriting')
+// let l
+// let pi=3.14
+// l=pi*d
+// console.log('aylananing uzunligi - ' +l)
+
+
+//5-masala
+// let a=+prompt ('kubning yon tomonini kiriting')
+// let v 
+// v=a*a*a
+// console.log('hajmi - ' +v)
+// let s
+// s=6*a*a
+// console.log('tola sirti - ' +s)
+
+
+//6masala
+// let a=+prompt('paralelepipedning tomonini kiriting')
+// let b=+prompt('paralelepipedning tomonini kiriting')
+// let c=+prompt('paralelepipedning tomonini kiriting')
+// let v
+// v=a*b*c
+// console.log('hajmi - ' +v)
+// let s 
+// s=2*(a*b + b*c + a*c)
+// console.log('tola sirti - ' +s )
+
+
+//7-masala
+// let r=+prompt('doiraning radiusini kiriting' )
+// pi=3.14
+// let l 
+// l=2*pi*r
+// console.log('uzunligi =' +l);
+// let s 
+// s=pi*r*r
+// console.log('yuzasi = ' +s);
+
+
+//8-masala
+// let a=+prompt('a tomopnini kiriting')
+// let b=+prompt('b tomopnini kiriting')
+// let c 
+// c=(a+b)/2
+// console.log('orta arifmetigi = ' + c);
+
+
+//9-masala
+
+
+
